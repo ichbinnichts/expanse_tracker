462099da69c61ccd840993650e3ee5d268c7220b
Nathan Faria <nathanfaria607@gmail.com>
2024-03-26 15:21:08 -0300

    feat: add expenses list

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+import 'package:expanse_tracker/expense/expenses_list.dart';
+import 'package:expanse_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -8,13 +10,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 20.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Nest course',
+      amount: 20.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Fast food',
+      amount: 35.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Book',
+      amount: 15.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list...'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
         ],
       ),
     );
