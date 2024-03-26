5493cec913792e644b76bb3a2fc3d3fd57a31086
Nathan Faria <nathanfaria607@gmail.com>
2024-03-26 13:51:30 -0300

    init

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:expanse_tracker/home/home.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
