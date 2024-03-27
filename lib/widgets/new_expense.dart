9de7d913f97961f3abc84c15485a0b199fbec5da
Nathan Faria <nathanfaria607@gmail.com>
2024-03-27 10:42:52 -0300

    feat: apply category dropdown button

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,6 +13,8 @@ class _NewExpenseState extends State<NewExpense> {
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
 
+  Category _selectedCategory = Category.leisure;
+
   void _presentDatePicker() async {
     final DateTime now = DateTime.now();
     final DateTime firstDate = DateTime(now.year - 10, now.month, now.day);
@@ -85,6 +87,27 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (category) => DropdownMenuItem(
+                          value: category,
+                          child: Text(
+                            category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
               OutlinedButton(
                 onPressed: () {
                   Navigator.pop(context);
