14f9c0afeeed5566e4f4d9d2e0398427cef45ae9
Nathan Faria <nathanfaria607@gmail.com>
2024-04-02 07:53:47 -0300

    feat: set up color scheme for app theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,21 @@
 import 'package:expanse_tracker/home/home.dart';
 import 'package:flutter/material.dart';
 
+final ColorScheme kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 35, 155, 253));
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
       debugShowCheckedModeBanner: false,
       home: const Home(),
     );
