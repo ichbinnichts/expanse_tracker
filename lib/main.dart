3cdf3464c980362d5594f9fd9913af56edd194ae
Nathan Faria <nathanfaria607@gmail.com>
2024-04-03 07:36:10 -0300

    refactor: add commented code that locks the screen app rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:expanse_tracker/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // Locking the screen rotation
+
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
+  //   runApp(const App());
+  // });
   runApp(const App());
 }
